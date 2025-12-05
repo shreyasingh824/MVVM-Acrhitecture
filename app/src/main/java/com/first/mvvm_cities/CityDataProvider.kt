0ab1ea660d04f733a69df673ab914d3a6eca0f3e
Shreya Singh <shreya.singh@tesco.com>
2025-12-05 14:33:14 +0530

    data class added

---
+++ app/src/main/java/com/first/mvvm_cities/CityDataProvider.kt
@@ -0,0 +1,20 @@
+package com.first.mvvm_cities
+
+class CityDataProvider {
+    private val cities = arrayListOf<city>()
+    init{
+        //here _ are space for clearity
+        cities.add(city("Bangkok", R.drawable.bangkok, 10_000_000))
+        cities.add(city("Beijing", R.drawable.beijing, 21_500_000))
+        cities.add(city("London", R.drawable.london, 8_900_000))
+        cities.add(city("New York", R.drawable.newyork, 10_000_000))
+        cities.add(city("Paris", R.drawable.bangkok, 10_000_000))
+        cities.add(city("Rio de Janeiro", R.drawable.rio, 10_000_000))
+        cities.add(city("Rome", R.drawable.rome, 10_000_000))
+        cities.add(city("Singapore", R.drawable.singapore, 10_000_000))
+        cities.add(city("Tokyo", R.drawable.tokyo, 10_000_000))
+    }
+
+    fun getCities() = cities
+
+}
---
+++ app/src/main/java/com/first/mvvm_cities/city.kt
@@ -0,0 +1,7 @@
+package com.first.mvvm_cities
+
+data class city (
+    val name: String,
+    val img: Int,
+    val population: Int
+)
